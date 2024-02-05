abd1c3f5556cd190736da8cf68ee9d9029fd7ed9
Hauke Bartsch <HaukeBartsch@gmail.com>
2024-02-05 16:12:14 +0100

    fix: inference in this version of fastMONAI needs to use a filename to apply the transformations correctly

--- SpineSegmentationInference.py
+++ SpineSegmentationInference.py
@@ -27,21 +27,50 @@ if not os.path.exists(output):
     except OSError as error:
         print(error)
 
-# load variables
+
 _, reorder, resample = load_variables(pkl_fn='data/SpineSegmentation/vars.pkl')
+learn_inf = load_learner('data/SpineSegmentation/spine_model.pkl',cpu=True)
 
-# load learner
-learn = load_learner('data/SpineSegmentation/spine_model.pkl')
+from scipy.ndimage import label
+from skimage.morphology import remove_small_objects
 
-mask = inference(learn, reorder, resample, datafolder)
+def pred_postprocess(pred_mask, avg_disc=10437, percentage=0.2):
+    small_objects = avg_disc * percentage
+    labeled_mask, ncomponents = label(pred_mask)
+    labeled_mask = remove_small_objects(labeled_mask, min_size=small_objects)
+    return np.where(labeled_mask>0, 1.0, 0.0)
 
-mask_obj = Series(datafolder)
-new_mask = mask.numpy()
-new_mask = new_mask.squeeze()
-new_mask = np.transpose(new_mask, (-1, 1, 0))
-new_mask = new_mask.copy()
-new_mask = new_mask.astype(np.uint16)
-mask_obj[:] = new_mask
+# alternative prediction
+# mask = inference(learn_inf, reorder, resample, datafolder)
+org_img, input_img, org_size = med_img_reader(datafolder, reorder, resample, only_tensor=False)
+# This is the only difference to the previous version, specify input as filename - not as input_img
+pred, *_ = learn_inf.predict(datafolder)
+pred_data = pred.data
+m = pred_data.numpy()
+m = m.squeeze()
+m = np.transpose(m, (-1,0,1))
+pred_data = m
+pred_mask = do_pad_or_crop(pred_data[None], input_img.shape[1:], padding_mode=0, mask_name=None)
+input_img.set_data(pred_mask)
+
+from torchio import Resize, Image
+resize = Resize(
+        org_size, 
+        image_interpolation='nearest',
+        label_interpolation='nearest'
+)
+input_img = resize(input_img)
+
+from SimpleITK import DICOMOrient, GetArrayFromImage
+orientation_itk = DICOMOrient(input_img.as_sitk(), ('').join(org_img.orientation))
+reoriented_array =  GetArrayFromImage(orientation_itk).transpose()
+reoriented_array = reoriented_array[None]
+org_img.set_data(reoriented_array)
+
+mask_data = org_img.data
+mask_data = torch.where(mask_data > 0.5, 1.0, 0.0)
+mask_data = torch.Tensor(pred_postprocess(mask_data))
+org_img.set_data(mask_data) # Here is the new mask data
 
 def save_series_pred(series_obj, save_dir, val='1234'):
     '''Make sure we get derived UIDs to allow for overwrite of image objects in PACS'''
@@ -58,5 +87,16 @@ def save_series_pred(series_obj, save_dir, val='1234'):
 
     series_obj.write(save_dir, opts={'keep_uid': True}, formats=['dicom'])
 
+mask = org_img
+mask_obj = Series(datafolder)
+new_mask = mask.numpy()
+new_mask = new_mask.squeeze()
+new_mask = np.transpose(new_mask, (-1, 1, 0))
+new_mask = new_mask.copy()
+new_mask = new_mask.astype(np.uint16)
+mask_obj[:] = new_mask
+
+if not("patientID" in mask_obj) or mask_obj.patientID == None or mask_obj.patientID == "":
+    mask_obj.patientID = "MeMyselfAndI"
 
 save_series_pred(mask_obj, output + '/mask')

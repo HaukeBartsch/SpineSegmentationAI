05c966a8953b050a9ea40612dedd087307591314
Hauke Bartsch <HaukeBartsch@gmail.com>
2024-02-08 18:13:03 +0100

    fix: flip the image upside-down and left-right before storing them as DICOM

--- SpineSegmentationInference.py
+++ SpineSegmentationInference.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''
    Do inference on a DICOM dataset.
+   ./SpineSegmentationInference.py data/Prediction/input data/Prediction/output
 '''
 
 import argparse
@@ -9,6 +10,7 @@ from fastMONAI.vision_all import *
 from imagedata.series import Series
 from skimage import measure
 import numpy as np
+import pandas as pd
 
 parser = argparse.ArgumentParser(
     prog='SpineSegmentation',
@@ -44,16 +46,18 @@ def pred_postprocess(pred_mask, avg_disc=10437, percentage=0.2):
 # mask = inference(learn_inf, reorder, resample, datafolder)
 org_img, input_img, org_size = med_img_reader(datafolder, reorder, resample, only_tensor=False)
 # This is the only difference to the previous version, specify input as filename - not as input_img
-pred, *_ = learn_inf.predict(datafolder)
+df = pd.Series(data={'image': datafolder}, index=['image'])
+pred, *_ = learn_inf.predict(df)
 pred_data = pred.data
 m = pred_data.numpy()
 m = m.squeeze()
 m = np.transpose(m, (-1,0,1))
-pred_data = m
+m = np.flip(m,axis=1)
+pred_data = m.copy()
 pred_mask = do_pad_or_crop(pred_data[None], input_img.shape[1:], padding_mode=0, mask_name=None)
 input_img.set_data(pred_mask)
 
-from torchio import Resize, Image
+from torchio import Resize
 resize = Resize(
         org_size, 
         image_interpolation='nearest',
@@ -74,14 +78,16 @@ org_img.set_data(mask_data) # Here is the new mask data
 
 def save_series_pred(series_obj, save_dir, val='1234'):
     '''Make sure we get derived UIDs to allow for overwrite of image objects in PACS'''
+    # the following changed in imagedata between the older 0.2 and the latest 0.3 version
     my_seriesInstanceUID = series_obj.seriesInstanceUID[:-4] + val
     series_obj.seriesInstanceUID = my_seriesInstanceUID
     my_studyID = series_obj.patientID[3:]
     series_obj.studyID = my_studyID
 
     for slice_idx in range(series_obj.slices):
-        my_SOPInstanceUID = series_obj.getDicomAttribute(
-            'SOPInstanceUID', slice=slice_idx)[:-4] + val
+        my_SOPInstanceUID = series_obj.SOPInstanceUIDs[0,slice_idx][:-4] + val
+        #my_SOPInstanceUID = series_obj.getDicomAttribute(
+        #    'SOPInstanceUID', slice=slice_idx)[:-4] + val
         series_obj.setDicomAttribute(
             'SOPInstanceUID', my_SOPInstanceUID, slice=slice_idx)
 
@@ -92,6 +98,9 @@ mask_obj = Series(datafolder)
 new_mask = mask.numpy()
 new_mask = new_mask.squeeze()
 new_mask = np.transpose(new_mask, (-1, 1, 0))
+# Attention, this is just not nice. DICOMOrient could have done something?
+new_mask = np.flip(new_mask, axis=0)
+new_mask = np.flip(new_mask, axis=1)
 new_mask = new_mask.copy()
 new_mask = new_mask.astype(np.uint16)
 mask_obj[:] = new_mask
